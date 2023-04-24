3168eec9816236c1e7b6ff8463909d5fb81e6fb7
shadowrunner11 <pieroramirez810@gmail.com>
2023-04-24 16:40:10 -0500

    :sparkles: added new column icon

--- src/components/GenericHeader/index.tsx
+++ src/components/GenericHeader/index.tsx
@@ -1,15 +1,20 @@
-import { TableCell, TableRow } from "@mui/material"
-import { ColumnConfig } from "../RulesTable/config"
+import { TableCell, TableRow, Typography, Stack} from "@mui/material"
+import { ColumnConfig } from '../RulesTable/config'
 import { memo } from "react"
 
 interface GenericHeaderProps {
-  data: readonly ColumnConfig[]
+  data: ColumnConfig[]
 }
 
 export const GenericHeader = memo(({data}: GenericHeaderProps)=>(
   <TableRow>
     {
-      data.map(({label})=> <TableCell key={label}>{label}</TableCell>)
+      data.map(({label, icon})=> <TableCell key={label} >
+        <Stack direction='row' gap={1}>
+          {icon}
+          <Typography>{label}</Typography>
+        </Stack>
+      </TableCell>)
     }
   </TableRow>
 ))
--- src/components/RulesTable/index.tsx
+++ src/components/RulesTable/index.tsx
@@ -1,5 +1,9 @@
 import { FolderOff, ReadMore } from '@mui/icons-material';
 import { CircularProgress, TableCell, TablePagination, TableRow, Typography } from '@mui/material'
+import relative from 'dayjs/plugin/relativeTime'
+import 'dayjs/locale/es'
+import dayjs from 'dayjs';
+
 import GenericTable from '../../layout/GenericTable'
 import { StatusIcon } from '../StatusIcon';
 import { GenericPopover } from '../GenericPopover';
@@ -10,7 +14,19 @@ import { useGetRulesStatus } from '../../hooks';
 import { GenericHeader } from '../GenericHeader';
 import { Info } from '../../layout/Info';
 
+dayjs.extend(relative)
+dayjs.locale('es')
 
+interface TimeAgoProps {
+  date: string | Date
+}
+
+const TimeAgo = ({date}: TimeAgoProps)=> <Typography>
+    {dayjs(date).from(new Date())}
+  </Typography>
+
+
+// TODO: check another abstraction for especial cases
 const EspecialConfigCell = ({resource, value, id} : {resource: string; value: unknown; id: string;})=>{
   if(resource === 'isActiveSonar')
     return <StatusIcon isActive={Boolean(value)}/>
@@ -23,6 +39,9 @@ const EspecialConfigCell = ({resource, value, id} : {resource: string; value: un
           dangerouslySetInnerHTML={{__html:String(value)}} 
         />
     } />)
+
+  if(resource === 'updated_at')
+    return <TimeAgo date={String(value)} />
 
   return <UncontrolledSwitch initialStatus={Boolean(value)} id={id}/>
 }
@@ -47,9 +66,12 @@ export function RulesTable(){
       body={<>
         {data?.map((result)=>(
           <TableRow key={result.id}>
-              {columns.map(({resource, especialConfig})=> {
+              {columns.map(({resource, especialConfig, icon})=> {
                 if(!especialConfig)
-                  return <TableCell key={resource+result.id}>{String(result[resource]?? '--')}</TableCell>
+                  return <TableCell 
+                    key={resource+result.id}>
+                      {String(result[resource]?? '--')}
+                    </TableCell>
 
                 return (
                   <TableCell key={resource+result.id}>
@@ -70,7 +92,7 @@ export function RulesTable(){
       footer={<TablePagination 
         rowsPerPage={10}
         count={total ?? 1000}
-        page={page ?? 1}
+        page={page - 1 ?? 1}
         onPageChange={()=> setPage(prev => prev + 1)}
       />}
     />
