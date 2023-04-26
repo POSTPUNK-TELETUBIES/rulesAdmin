f2b89ff51e4db8a582458cbc81bd47753ac8aa8f
brandt322 <bgamboaa98@gmail.com>
2023-04-26 11:45:02 -0500

    Added relative time

--- src/components/TimeAgo/index.tsx
+++ src/components/TimeAgo/index.tsx
@@ -1,23 +1,27 @@
 import { Typography } from "@mui/material";
 
 import relative from "dayjs/plugin/relativeTime";
+import updateLocale from "dayjs/plugin/updateLocale";
 import "dayjs/locale/es";
 import dayjs from "dayjs";
-import { options } from "./config";
-import { replaceByDict } from "../../tools";
 
-dayjs.extend(relative);
 dayjs.locale("es");
+dayjs.extend(relative);
+dayjs.extend(updateLocale);
 
 interface TimeAgoProps {
   date: string | Date;
 }
 
+dayjs.updateLocale("es", {
+  relativeTime: {
+    s: "1 s",
+    m: "1 min",
+    h: "1 h",
+    d: "1 d",
+  },
+});
+
 export const TimeAgo = ({ date }: TimeAgoProps) => (
-  <Typography align="right">
-    {replaceByDict(dayjs(date).from(new Date()), options).replace(
-      /una|un/,
-      "1"
-    )}
-  </Typography>
+  <Typography align="right">{dayjs(date).from(new Date())}</Typography>
 );
