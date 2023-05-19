849e39d99df6d6800921dd426d2da2501d318ee3
RaidCr13 <raidcr13.17@gmail.com>
2023-05-19 18:13:43 -0500

    :sparkles: Add modal on synchro button

--- src/components/SynchroButton/index.tsx
+++ src/components/SynchroButton/index.tsx
@@ -1,16 +1,75 @@
 import { Sync } from "@mui/icons-material";
-import { ListItemIcon, ListItemText, MenuItem } from "@mui/material";
+import {
+  ListItemIcon,
+  ListItemText,
+  MenuItem,
+  Button,
+  Modal,
+  Typography,
+  Box,
+} from "@mui/material";
 import { useSynchro } from "../../hooks";
+import { useState } from "react";
+
+const style = {
+  position: "absolute" as const,
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  pt: 2,
+  px: 4,
+  pb: 3,
+};
 
 export const SynchroButton = () => {
   const [_handleClickSyncro] = useSynchro();
+  const [open, setOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setOpen(false);
+  };
+
+  const handleSyncro = () => {
+    _handleClickSyncro();
+    handleCloseModal();
+  };
 
   return (
-    <MenuItem onClick={_handleClickSyncro}>
-      <ListItemIcon>
-        <Sync fontSize="small" />
-      </ListItemIcon>
-      <ListItemText>Sincronizar</ListItemText>
-    </MenuItem>
+    <>
+      <MenuItem onClick={handleOpenModal}>
+        <ListItemIcon>
+          <Sync fontSize="small" />
+        </ListItemIcon>
+        <ListItemText>Sincronizar</ListItemText>
+      </MenuItem>
+
+      <Modal
+        open={open}
+        onClose={handleCloseModal}
+        aria-labelledby="child-modal-title"
+        aria-describedby="child-modal-description"
+      >
+        <Box sx={{ ...style, width: 350 }}>
+          <Typography variant="h6">Confirmar sincronización</Typography>
+          <Typography>
+            ¿Estás seguro de que deseas sincronizar los datos?
+          </Typography>
+          <Button onClick={handleCloseModal} color="primary">
+            Cancelar
+          </Button>
+          <Button onClick={handleSyncro} color="primary" autoFocus>
+            Sincronizar
+          </Button>
+        </Box>
+      </Modal>
+    </>
   );
 };
