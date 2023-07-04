54b8c2611069f5e11e148aca6819840d80364007
jaen <jaenfigueroa03042000@gmail.com>
2023-07-04 12:37:41 -0500

    refactor: improve "timeAgo"

--- src/tools/getTimeAgo.ts
+++ src/tools/getTimeAgo.ts
@@ -1,4 +1,3 @@
-import { Typography } from '@mui/material';
 import updateLocale from 'dayjs/plugin/updateLocale';
 import relative from 'dayjs/plugin/relativeTime';
 import 'dayjs/locale/es';
@@ -8,11 +7,6 @@ dayjs.extend(updateLocale);
 dayjs.locale('es');
 dayjs.extend(relative);
 
-interface TimeAgoProps {
-  date: string | Date;
-}
-
-//TODO: Buscar la manera de que el singleton de dayjs tenga su propio archivo
 dayjs.updateLocale('es', {
   relativeTime: {
     future: 'en %s',
@@ -31,6 +25,6 @@ dayjs.updateLocale('es', {
   },
 });
 
-export const TimeAgo = ({ date }: TimeAgoProps) => (
-  <Typography align='left'>{dayjs(date).from(new Date())}</Typography>
-);
+export const getTimeAgo = (date: string | Date) => {
+  return dayjs(date).from(new Date());
+};
