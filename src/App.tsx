6a004dc0551dcd31c5adaa303641c0a9b2fb96f2
Aldair Chuman <aldair@DESKTOP-T7OTJRV.localdomain>
2023-07-24 12:02:03 -0500

    feat: agregar React Auth Kit

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,21 @@ import { SnackbarProvider } from 'notistack';
 import { ColorModeWrapper } from './theme';
 import { AppRoutes } from './routes/AppRoutes';
 import { Box } from '@mui/material';
+import { AuthProvider } from 'react-auth-kit';
 
 function App() {
   return (
     <Box display={'flex'} flexDirection={'column'} minHeight={'100vh'}>
       <SnackbarProvider>
         <ColorModeWrapper>
-          <AppRoutes />
+          <AuthProvider
+            authType={'cookie'}
+            authName={'_auth'}
+            cookieDomain={window.location.hostname}
+            cookieSecure={window.location.protocol === 'https:'}
+          >
+            <AppRoutes />
+          </AuthProvider>
         </ColorModeWrapper>
       </SnackbarProvider>
     </Box>
